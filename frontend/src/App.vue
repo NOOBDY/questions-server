48f4125b60a45b5d425b5c7f0d7eee8718b3afbc
NOOBDY <htommyf12@gmail.com>
2021-01-07 06:39:42 +0800

    got vue sort of working

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <GetQuestions/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import GetQuestions from './components/GetQuestions.vue'
 
 export default {
   name: 'App',
   components: {
+    GetQuestions,
     HelloWorld
   }
 }
---
+++ frontend/src/components/GetQuestions.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Total Value Packages: {{ totalVuePackages }}</div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "GetQuestions",
+  data() {
+    return {
+      totalVuePackages: null,
+    };
+  },
+  async created() {
+    const response = await axios.get("https://api.npms.io/v2/search?q=vue");
+    this.totalVuePackages = response.data.total;
+  },
+};
+</script>
