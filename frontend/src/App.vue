a4f7bdff6eeadee5fb33ae1c6326296a4a20a45d
NOOBDY <htommyf12@gmail.com>
2021-01-16 11:26:36 +0800

    listed all questions as cards

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,39 @@
 <template>
   <div id="app">
-    <get-questions />
+    <div class="container flex-container">
+      <div
+        v-for="(question, index) in questions"
+        :key="index"
+        class="container m-1 question"
+      >
+        <question :question="question" :index="index" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import GetQuestions from "./components/GetQuestions.vue";
+import axios from "axios";
+import Question from "./components/Question.vue";
 
 export default {
   name: "App",
   components: {
-    GetQuestions,
+    Question,
+  },
+  data() {
+    return {
+      questions: null,
+    };
+  },
+  async created() {
+    this.questions = await this.getQuestions();
+  },
+  methods: {
+    async getQuestions() {
+      const response = await axios.get("http://localhost:9090/questions");
+      return response.data;
+    },
   },
 };
 </script>
@@ -21,7 +44,22 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+}
+.question {
+  width: 300px;
+  border-radius: 20px;
+  background-color: #1c1c2e;
+  box-shadow: -1rem 0 3rem #000;
+  margin: 30px;
+}
+.question:hover {
+  transform: translate(-0.5rem, -0.5rem);
+  transition: 100ms;
+}
+.flex-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
 }
 </style>
---
+++ frontend/src/components/Question.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="text">
+    <p>Question {{ index + 1 }}: {{ question.question }}</p>
+    <ul class="list-group">
+      <li class="btn option">A: {{ question.option_a }}</li>
+      <li class="btn option">B: {{ question.option_b }}</li>
+      <li class="btn option">C: {{ question.option_c }}</li>
+      <li class="btn option">D: {{ question.option_d }}</li>
+    </ul>
+    <button class="btn" id="answer-btn" v-on:click="show = !show">
+      Show Answer
+    </button>
+    <p v-if="show">{{ question.answer }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["question", "index"],
+  data() {
+    return {
+      show: false,
+    };
+  },
+};
+</script>
+
+<style>
+.option {
+  text-align: left;
+  margin: 5px;
+  background-color: #1c1c2e;
+}
+.text {
+  padding: 20px;
+  margin: 20px;
+  color: #b2b5cc;
+}
+.btn {
+  color: #b2b5cc;
+  background-color: transparent;
+  border-color: turquoise;
+  border-radius: 20px;
+  transition: 30ms;
+}
+.btn:hover {
+  color: white;
+  background-color: turquoise;
+}
+#answer-btn {
+  margin: 10px;
+}
+</style>
